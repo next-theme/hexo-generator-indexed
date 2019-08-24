f39817bb7074418af6862c1074f02cb39bdf968a
StevenJoeZhang <1119186082@qq.com>
2019-08-24 11:01:22 +0800

    Use lodash/merge

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 'use strict';
 
-hexo.config.index_generator = Object.assign({
+const merge = require('lodash/merge');
+
+hexo.config.index_generator = merge({
   per_page: typeof hexo.config.per_page === 'undefined' ? 10 : hexo.config.per_page,
   order_by: '-date'
 }, hexo.config.index_generator);
