80c3a7f8eb8bd2f1fcbcf40aee326238c1b4a759
StevenJoeZhang <1119186082@qq.com>
2019-10-09 15:38:48 +0800

    Use Object.assigin

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 
 'use strict';
 
-const { merge } = require('lodash');
-
-hexo.config.index_generator = merge({
+hexo.config.index_generator = Object.assign({
   per_page: typeof hexo.config.per_page === 'undefined' ? 10 : hexo.config.per_page,
   order_by: '-date'
 }, hexo.config.index_generator);
